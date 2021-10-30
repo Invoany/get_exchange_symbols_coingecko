e8031234154ae47274b9646fcc1a491a7b5c0178
Jorge Nunes <63741348+Invoany@users.noreply.github.com>
2021-10-30 17:44:00 +0100

    Add files via upload

---
+++ getExchangeCoinGecko.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import pandas as pd
+from datetime import datetime
+
+# The following function retrieves all the Exchanges names and ID's identified by CoinGecko
+
+def get_exchange_symbols_coingecko():
+    base_url = "https://api.coingecko.com/api/v3/"
+    print("({}) Geting all Exchanges from Coin Gecko".format(str(datetime.today().strftime("%H:%M:%S:%f"))))
+    req = requests.get(base_url + "/exchanges/list")
+    res = json.loads(req.text)
+    df_exchange_symbols = pd.DataFrame.from_dict(res) 
+    df_exchange_symbols.index.name = 'index'
+    try:
+        df_exchange_symbols.to_csv('Output\AllExchanges_CoinGecko_{}.csv'.format(str(datetime.today().strftime('%Y%m%d'))))
+    except:
+        print("It was not possible to save/create the file")
+    print("({}) End of get_exchange_symbols_coingecko".format(str(datetime.today().strftime("%H:%M:%S:%f"))))
+    return df_exchange_symbols
+
+df_exchange_symbols = get_exchange_symbols_coingecko()
